5d21e2041e328ca8c94de382d688aefe9b015f76
Yurii <diesel6@i.ua>
2019-05-31 13:28:23 +0300

    add some tasks to gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+const gulp = require('gulp');
+
+
+gulp.task('message', async function() {
+    console.log("HTTP Server Started");
+  });
+
+  gulp.task('default', async function() {
+    console.log("HTTP Server Started");
+  });
