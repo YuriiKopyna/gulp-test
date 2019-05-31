f0b30c8ea80b7b23c31042a7c79d6b70045e32e3
Yurii <diesel6@i.ua>
2019-05-31 13:38:12 +0300

    copy files by using task in gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,15 @@ const gulp = require('gulp');
 
 gulp.task('message', async function() {
     console.log("HTTP Server Started");
-  });
+});
 
-  gulp.task('default', async function() {
+gulp.task('default', async function() {
     console.log("HTTP Server Started");
-  });
+});
+
+//Copying html file
+
+gulp.task('copyHtml', async function() {
+   gulp.src('src/*.html')
+   .pipe(gulp.dest('dist'));
+});
