b77db6ef4d45cf4615d0ed01d74071847fa765e9
Yurii <diesel6@i.ua>
2019-05-31 16:51:53 +0300

    delete dist folder, modify gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -3,27 +3,18 @@ const imagemin = require('gulp-imagemin');
 const uglify = require('gulp-uglify');
 const sass = require('gulp-sass');
 
-gulp.task('message', async function() {
-    console.log("HTTP Server Started");
-});
-
-gulp.task('default', async function() {
-    console.log("HTTP Server Started");
-});
-
 //Copying html file
-
 gulp.task('copyHtml', async function() {
    gulp.src('src/*.html')
    .pipe(gulp.dest('dist'));
 });
 
 //Optimize Images
-gulp.task('imageMin', () =>
+gulp.task('imageMin', async function() {
 	gulp.src('src/images/*')
 		.pipe(imagemin())
 		.pipe(gulp.dest('dist/images'))
-);
+});
 //Optimize JavaScript
 gulp.task('minify', async function() {
     gulp.src('src/js/*.js')
@@ -31,8 +22,10 @@ gulp.task('minify', async function() {
         .pipe(gulp.dest('dist/js'))
 });
 //Compile sass
-gulp.task('sass', () => 
+gulp.task('sass', async function() { 
     gulp.src('src/sass/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('dist/css'))
-);
+});
+//All tasks in 1 command
+gulp.task('default', ['copyHtml', 'imageMin', 'minify', 'sass']);
